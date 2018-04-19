43809049c41287c64409e182c01008c4234111f3
Tarikul Mannan <tarikul.mannan@iut-dhaka.edu>
2018-04-20 00:03:43 +0600

    full bus implementation

--- src/bus.h
+++ src/bus.h
@@ -2,15 +2,25 @@
 #define BUS_H
 #include <QString>
 
-
 class Bus
 {
-public:
-    Bus(QString _busname, QString _origin, QString _dest, QString _type);
+ private:
     QString busname;
     QString origin;
     QString dest;
     QString type;
+
+public:
+    Bus(QString _busname, QString _origin, QString _dest, QString _type);
+    QString getBusname();
+    QString getOrigin();
+    QString getDest();
+    QString getType();
+    void setBusname();
+    void setOrigin();
+    void setDest();
+    void setType();
+    void updateBus();
 };
 
 #endif // BUS_H
