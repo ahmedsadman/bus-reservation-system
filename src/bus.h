c3efee5b8a6b5db99b1f0f5f4ed285991cfd1588
Tarik-27 <35687687+Tarik-27@users.noreply.github.com>
2018-04-20 00:30:06 +0600

    Full bus implementation (#4)
    * full bus implementation
    
    * Fixed Bus class mistakes

--- src/bus.h
+++ src/bus.h
@@ -2,15 +2,25 @@
 #define BUS_H
 #include <QString>
 
-
 class Bus
 {
-public:
-    Bus(QString _busname, QString _origin, QString _dest, QString _type);
+ private:
     QString busname;
     QString origin;
     QString dest;
     QString type;
+
+public:
+    Bus(QString _busname, QString _origin, QString _dest, QString _type);
+    QString getBusname();
+    QString getOrigin();
+    QString getDest();
+    QString getType();
+    void setBusname(QString _busname);
+    void setOrigin(QString _origin);
+    void setDest(QString _dest);
+    void setType(QString _type);
+    void updateBus();
 };
 
 #endif // BUS_H
